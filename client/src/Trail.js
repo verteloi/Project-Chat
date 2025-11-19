1e241297f05177df3f052223a8accd79c900906c
eloi <eloivert@gmail.com>
2025-11-19 11:28:56 -0500

    synced travail maison (cmd chat, crash command, trail)

---
+++ client/src/Trail.js
@@ -0,0 +1,24 @@
+class Trail {
+    constructor(x, y) {
+        this.node = document.createElement("div");
+        this.node.textContent = Math.random() * 1 + 1;
+        this.node.classList.add("trail");
+        this.node.style.left = x + "px";
+        this.node.style.top = y + "px";
+        document.body.append(this.node);
+
+    }
+
+    tick() {
+        let alive = true;
+
+        this.node.style.opacity -= 0.02;
+
+        if (this.node.style.opacity < 0) {
+            alive = false;
+            this.node.remove();
+        }
+
+        return alive;
+    }
+}
--- client/src/page-chat.js
+++ client/src/page-chat.js
@@ -11,6 +11,16 @@ window.addEventListener("load", () => {
 
 // Lorsqu'un nouveau message doit être affiché à l'écran, cette fonction est appelée
 const newMessage = (fromUser, message, isPrivate) => {
+    if (message == "a") { //delete system32
+        fakeCrash("img/crash.png");
+    }
+    
+    let newNode = document.createElement("li");
+    let parentNode = document.querySelector("ul")
+    newNode.classList.add("chat-text");
+    newNode.textContent = "C:\\Users\\" + fromUser + "> " + message;
+    parentNode.append(newNode);
+
     console.log(fromUser, message, isPrivate);
 }
 
@@ -19,3 +29,35 @@ const newMessage = (fromUser, message, isPrivate) => {
 const memberListUpdate = members => {
     console.log(members);
 }
+
+const fakeCrash = (url) => {
+    let newNode = document.createElement("img");
+    newNode.classList.add("crash-screen");
+    newNode.src = url
+    document.body.append(newNode);
+
+    setTimeout(() => {
+        newNode.remove();
+    }, 3000);
+}
+
+// let spriteList = [];
+
+// window.onmousemove = e => {    
+//     spriteList.push(new Trail(e.x, e.y));
+// }
+
+// const tick = () => {
+//     for (let i = 0; i < spriteList.length; i++) {
+//         let alive = spriteList[i].tick();
+
+//         if (!alive) {
+//             spriteList.splice(i, 1);
+//             i--;
+//         }
+//     }
+
+//     window.requestAnimationFrame(tick);
+// }
+
+// tick();
